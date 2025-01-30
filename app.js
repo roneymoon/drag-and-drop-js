491a0aaea2566ff1a311e33e152c497f7b5cc325
Roney Moon <112367082+roneymoon@users.noreply.github.com>
2025-01-30 20:35:56 +0530

    added the files

---
+++ app.js
@@ -0,0 +1,39 @@
+const dragables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
+
+dragables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", () => {
+        draggable.classList.add("dragging");
+    });
+});
+
+dragables.forEach((draggable) => {
+    draggable.addEventListener("dragend", () => {
+        draggable.classList.remove("dragging");
+    });
+});
+
+containers.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const afterElement = getAfterElement(container, e.clientY);
+        console.log(afterElement);
+        const draggable = document.querySelector(".dragging");
+        container.insertBefore(draggable, afterElement);
+    });
+});
+
+function getAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll(".draggable:not(.dragging)")];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        // console.log(offset);
+        if(offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        }
+        return closest;
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
